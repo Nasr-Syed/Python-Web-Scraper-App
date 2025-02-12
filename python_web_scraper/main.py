814508c00375a38f27a6e1248cf1d05349cba511
Nasr Syed <nasrsyed20@gmail.com>
2025-02-11 23:16:51 -0500

    starting project - added comments

--- python_web_scraper/main.py
+++ python_web_scraper/main.py
@@ -1,4 +1,9 @@
-# Welcome to my Python web scraper
-
+'''Welcome to my Python Web Scraper
+in this project I will test web scraping from Indeed job searches, to gain insights and gather job data for faster, effective understanding of the job market.
+'''
 # built by Nasr Syed
+
+
+
+
 
